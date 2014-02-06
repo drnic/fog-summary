2b5f4141988e40f40b3db088c77bfc9c4349993c
Dr Nic Williams <drnicwilliams@gmail.com>
2014-02-06 10:42:57 -0500

    initial scaffold

---
+++ lib/fog/summary.rb
@@ -0,0 +1,7 @@
+require "fog/summary/version"
+
+module Fog
+  module Summary
+    # Your code goes here...
+  end
+end
---
+++ lib/fog/summary/version.rb
@@ -0,0 +1,5 @@
+module Fog
+  module Summary
+    VERSION = "0.0.1"
+  end
+end
